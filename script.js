691db4b7524d035741c1c4e884bf7b07fb458887
nemili83 <nemili83@gmail.com>
2023-02-14 15:09:35 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,28 @@
-const api_key = "55b567790d8e4eb2f61aee05734f0e86";
-const submitBtn = document.getElementById("submit-btn");
+const searchBox = document.querySelector('.search-box');
+const searchBar = document.querySelector('.search-bar');
+const searchBtn = document.querySelector('.search-btn');
+const locationBox = document.querySelector('.location');
+const weatherBox = document.querySelector('.weather');
+const temperatureBox = document.querySelector('.temperature');
+const iconBox = document.querySelector('.icon');
 
-submitBtn.addEventListener("click", () => {
-	const cityInput = document.getElementById("city-input").value;
-	const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput}&units=metric&appid=${api_key}`;
-
-	fetch(url)
-		.then(response => response.json())
-		.then(data => {
-			if (data.main && data.main.temp) {
-				const temperature = Math.round(data.main.temp);
-				const description = data.weather[0].description;
-				document.getElementById("result").innerHTML = `Temperature: ${temperature} &deg;C<br>Description: ${description}`;
-				document.getElementById("result").style.display = "block";
-			} else {
-				document.getElementById("result").innerHTML = "Unable to get weather data. Please check the city name and try again.";
-				document.getElementById("result").style.display = "block";
-			}
-		})
-		.catch(error => console.log(error));
+searchBtn.addEventListener('click', function() {
+  getWeather(searchBar.value);
 });
+
+function getWeather(city) {
+  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=55b567790d8e4eb2f61aee05734f0e86&units=metric`)
+    .then(response => response.json())
+    .then(data => {
+      const location = `${data.name}, ${data.sys.country}`;
+      const weather = data.weather[0].description;
+      const temperature = `${data.main.temp}°C`;
+      const icon = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+
+      locationBox.textContent = location;
+      weatherBox.textContent = weather;
+      temperatureBox.textContent = temperature;
+      iconBox.innerHTML = `<img src="${icon}" alt="${weather}">`;
+    })
+    .catch(error => console.error(error));
+}
